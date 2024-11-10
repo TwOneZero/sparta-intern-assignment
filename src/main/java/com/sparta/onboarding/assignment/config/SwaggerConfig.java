41af19358c1caa171fb4d3fd88a6a60baf3dbebc
twonezero <dnjsdudtm98@naver.com>
2024-11-10 15:22:34 +0900

    feat: Swagger 설정 추가

---
+++ src/main/java/com/sparta/onboarding/assignment/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.sparta.onboarding.assignment.config;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Sparta Onboarding - Security, Jwt", version = "1.0.0", description = "Jwt, Security 를 활용한 인증, 인가 테스트")
+)
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"))
+                .addServersItem(new Server().url("http://localhost:8080"));
+    }
+}
